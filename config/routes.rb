bc576aaccd0577e4c6d428c8242a2235ff0bd52f
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-26 12:39:59 +0100

    Add API endpoint for customer login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # devise_for :customers, controllers: {
-      #   registrations: 'api/v1/customers/registrations',
-      #   # sessions: 'api/v1/customers/sessions'
-      # }
+ 
       devise_for :customers, controllers: {
         registrations: 'api/v1/customers/registrations',
-        sessions: 'api/v1/sessions'
+        sessions: 'api/v1/customers/sessions'
       }
     end
   end
