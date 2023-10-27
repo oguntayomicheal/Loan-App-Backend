a819e5cd6dadb17fabae6ff9b9c74678e80e1378
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-27 19:47:10 +0100

    Add attritue accessor to loan_application model

--- app/models/loan_application.rb
+++ app/models/loan_application.rb
@@ -1,3 +1,7 @@
 class LoanApplication < ApplicationRecord
+  attr_accessor :status
+  attr_accessor :details
+  attr_accessor :repayment_schedule
+  attr_accessor :amount_to_pay
   belongs_to :customer
 end
