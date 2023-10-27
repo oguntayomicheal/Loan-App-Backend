c32c5cb77c4c8c8cee0e16905528ea97ee541eb0
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-27 19:46:02 +0100

    Add columns for status, details, repayment_schedule, amount_to_pay to loan_application table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_26_153332) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_27_184154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_26_153332) do
     t.bigint "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
+    t.text "details"
+    t.text "repayment_schedule"
+    t.decimal "amount_to_pay"
     t.index ["customer_id"], name: "index_loan_applications_on_customer_id"
   end
 
