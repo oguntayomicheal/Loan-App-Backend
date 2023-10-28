c962b343b3113bd51f820e906a1f7242f5780697
oguntayomicheal <oguntayomicheal@gmail.com>
2023-10-28 04:40:35 +0100

    Update index action in loan applications controller for staff

--- app/controllers/api/v1/staffs/loan_applications_controller.rb
+++ app/controllers/api/v1/staffs/loan_applications_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::Staffs::LoanApplicationsController < ApplicationController
+    def index
+      @loan_applications = LoanApplication.select(:id, :customer_name, :loan_amount, :status)
+                                         
+      render json: @loan_applications
+    end
 end
