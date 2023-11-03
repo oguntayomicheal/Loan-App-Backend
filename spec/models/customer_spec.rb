dc333d6c43555e864e10b0b6881047e02c5eb411
oguntayomicheal <oguntayomicheal@gmail.com>
2023-11-03 15:21:20 +0100

    Add test for customer  model

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  subject do
+    Customer.new(
+      email: 'test@example.com',
+      password: 'password'
+    )
+   
+  end
+
+  before { subject.save }
+
+  describe 'validations' do
+    it 'should be valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'Testing Associations' do
+    it 'has many loan_applications' do
+      assoc = Customer.reflect_on_association(:loan_applications)
+      expect(assoc.macro).to eq :has_many
+    end
+
+
+  end
+
+
+
+
+end
